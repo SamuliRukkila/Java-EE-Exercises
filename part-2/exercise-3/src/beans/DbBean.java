bc2b2c85e0d68b0d3be3b4e49876d40bc7b872a4
SamuliRukkila <samuli.rukkila@hotmail.fi>
2019-07-03 16:32:52 +0300

    Tehtävä 2.3 tehty; jatketaan tehtävään 2.4

--- part-2/exercise-3/src/beans/DbBean.java
+++ part-2/exercise-3/src/beans/DbBean.java
@@ -24,13 +24,14 @@ package beans;
 
 import java.io.Serializable;
 import java.sql.Connection;
-import java.sql.DriverManager;
-//import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.ResultSet;
 
-//import java.sql.Statement;
+import java.sql.Statement;
 import javax.enterprise.context.Dependent;
 import javax.inject.Named;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
 
 /* @Dependent (riippuvainen) tarkoittaa että beanilla ei ole varsinaista scopea, vaan
 DbBean on käytössä silloin kun toinen bean tarvitsee sitä */
@@ -46,81 +47,81 @@ public class DbBean implements Serializable {
     this.message = "";
   }
 
-  /*
-   * Yhteydenotto kantaan vanhaan JDBC-tyyliin
+  /**
+   * Creates a connection to database called jeedb2 using Payara's Connection 
+   * Pool. After the connection has been established, it'll be returned to 
+   * function who called it.
+   * 
+   * @return conn - Connection to MySQL
    */
   public Connection Connect() {
 
     Connection conn;
-
+    
     try {
-      Class.forName("com.mysql.jdbc.Driver");
-
-    } catch (ClassNotFoundException cnfe) {
-      System.out.println(cnfe);
-    }
-
-    // tietokannan osoite ja tietokannan nimi
-    String url = "jdbc:mysql://localhost/kannan_nimi";
-
-    try {
-      // kannan tunnari ja salasana
-      conn = (Connection) DriverManager.getConnection(url, "root", "root");
-
-      return conn;
-
-    } catch (SQLException e) {
-      System.out.println(e);
+      Context ctx = new InitialContext();
+      DataSource ds = (DataSource) ctx.lookup("jdbc/mysql_2_3");
+      conn = ds.getConnection();
+    } catch (Exception ex) {
+      System.out.println(ex);
       return null;
-
     }
-
+      return conn;
   }
 
-  /*
-   * Haetaan kannasta dataa sisääntulevan tiedon perusteella palauttaa String
-   * message tai null, riippuen siitä onnistuuko kysely. Avaa importeissa ja alla
-   * olevat kommentit ja luo taulu kantaan, niin sovellus toimii kannan kanssa.
+  /**
+   * Generates a message telling if wanted username is being taken already
+   * 
+   * @param username which'll be checked
+   * @return message telling if username is taken already
    */
   public String getMessage(String value) {
 
     try {
-      // Connection conn = Connect();
-
-      // Tässä tietokantoperaatiot joilla haetaan dataa users-taulusta
-
-      // Statement stmt = conn.createStatement();
-      // ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username LIKE '%"
-      // + value + "%'");
-
-      // int rivilkm = 0;
-      // while (rs.next()) {
-      // rivilkm++;
-      // }
-
-      /*
-       * Tämä esimerkki on laitettu toimimaan niin ettei tarvitse käyttää tietokantaa.
-       * Kokeile syöttää käyttöliittymän username-kenttään "username" tai jokin muu
-       * arvo
-       */
-      if (value.equals("username")) {
-        // if (rivilkm == 1) {
-
-        message = "On jo käytössä, valitse joku muu!";
-      } else {
-
-        message = "Käytettävissä!";
+      Connection conn = Connect();
+      Statement stmt = conn.createStatement();
+      ResultSet rs = stmt.executeQuery("SELECT * FROM users WHERE username='" + value + "';");
+      
+      int rivilkm = 0;
+      while (rs.next()) {
+        rivilkm++;
       }
-
+      
+      if (rivilkm == 1) {
+        message = "On jo käytössä. Valitse toinen käyttäjätunnus";
+      } else {
+        message = "Käytettävissä";
+      }
       return message;
 
     } catch (Exception ex) {
-
       System.out.println("Tuli poikkeus" + ex);
-
       return null;
     }
-
+  }
+  
+  /**
+   * Creates new user (=credentials)
+   * 
+   * After connecting to MySQL, function will execute a new query which'll
+   * try to insert new credentials into the table. Message will generated too
+   * telling if insertion was successful or not.
+   * 
+   * @param username New username
+   * @param password New password
+   * @return message telling if insertion was successful or not
+   */
+  public String createCredentials(String username, String password) {
+    try {
+      Connection conn = Connect();
+      Statement stmt = conn.createStatement();
+      stmt.execute("INSERT INTO `users` (`username`, `password`)"
+          + "VALUES ('" + username + "','" + password + "');");
+      return "Tunnukset luotu";
+    } catch (Exception ex) {
+      System.out.println(ex);
+      return "Tunnareita ei voitu luoda" + ex;
+    }
   }
 
 }
--- part-2/exercise-3/src/beans/UserBean.java
+++ part-2/exercise-3/src/beans/UserBean.java
@@ -7,6 +7,7 @@
 package beans;
 
 import java.io.Serializable;
+import java.util.Random;
 
 import javax.enterprise.context.SessionScoped;
 import javax.inject.Inject;
@@ -24,34 +25,95 @@ public class UserBean implements Serializable {
   private String username;
   private String password;
   private String ajaxmessage;
-  // CDI -injektio tapahtuu tässä:
+  private String notice;
+
+  // Injection to DBBean - meaning we can use the functions and 
+  // variables in DbBean.
   @Inject
   private DbBean dbBean;
-
+  
+  /**
+   * Sends credentials (username + password) forward to DBBean which'll
+   * insert those values into table. If the insertion is successful ->
+   * username, password + ajaxmessage will be emptied for another insertion.
+   * 
+   * If the insertion fails, a error message will be printed for user.
+   * @return notice - message which tell if adding new user was successful or not
+   */
+  public String sendCredentials() {
+    notice = dbBean.createCredentials(username, password);
+    if (notice == "Tunnukset luotu") {
+      username = "";
+      password = "";
+      ajaxmessage = "";
+    }
+    return notice;
+  }
+  
+  /**
+   * Using DbBean, this function will determine if wanted username is taken already.
+   * @return ajaxmessage - telling if username is taken already
+   */
   public String getAjaxmessage() {
-    // UserBeaniin käyttöliittymästä tullut username
-    // annetaan DbBeanin getMessage -metodille joka palauttaa viestin
-    ajaxmessage = dbBean.getMessage(username);
-
+    if (username != null) {
+      if (!username.isEmpty()) {
+        ajaxmessage = dbBean.getMessage(username);
+      }
+    } else {
+      ajaxmessage = null;
+    }
     return ajaxmessage;
   }
 
+  /**
+   * Set new ajaxmessage
+   * @param ajaxmessage New ajaxmessage
+   */
   public void setAjaxmessage(String ajaxmessage) {
     this.ajaxmessage = ajaxmessage;
   }
-
+  
+  /**
+   * Get notice -message
+   * @return notice
+   */
+  public String getNotice() {
+    return this.notice;
+  }
+  /**
+   * Sets new notice -message
+   * @param new notice message
+   */
+  public void setNotice(String notice) {
+    this.notice = notice;
+  }
+  
+  /**
+   * Get password -message
+   * @return password
+   */
   public String getPassword() {
     return password;
   }
-
+  /**
+   * Sets new password
+   * @param new password
+   */
   public void setPassword(String password) {
     this.password = password;
   }
-
+  
+  /**
+   * Get username
+   * @return username
+   */
   public String getUsername() {
     return username;
   }
-
+  /**
+   * Sets new username
+   * @param new username
+   */
   public void setUsername(String username) {
     this.username = username;
   }
