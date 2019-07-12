befb93b99e1626847adb003b5b000b4aca312461
SamuliRukkila <samuli.rukkila@hotmail.fi>
2019-07-13 00:38:08 +0300

    Tehtävä 3.1 kommentoitu => jatketaan tehtävään 3.2

--- part-3/exercise-1/src/main/java/jpaclasses/JPAsimplekoe.java
+++ part-3/exercise-1/src/main/java/jpaclasses/JPAsimplekoe.java
@@ -45,26 +45,47 @@ import javax.persistence.Query;
 @SuppressWarnings("unchecked")
 public class JPAsimplekoe {
   
+  // Objects which we'll use with JPA to perform queries, updates etc
 	private static EntityManagerFactory emf;
 	private static EntityManager em;
 	private static EntityTransaction tx;
+	
 	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	
+	/**
+	 * Init's entity manager according to same manager-factory in persistence.xml, where it 
+	 * has been created.
+	 * @throws Exception Throws error if cannot init entity-manager
+	 */
 	public static void initEntityManager() throws Exception {
 		emf = Persistence.createEntityManagerFactory("JPAsimplekoePU");
 		em = emf.createEntityManager();
 	}
-
+	
+	/**
+	 * Closes all JPA -connections to MySQL
+	 */
 	public static void closeEntityManager() {
 		em.close();
 		emf.close();
 	}
-
+	
+	/**
+	 * Starts transaction. After transaction has been established, queries can be executed.
+	 * Transaction -initialization -object will be placed to global variable called "tx"
+	 */
 	public static void initTransaction() {
 		tx = em.getTransaction();
 	}
 	
-	public static int printBookGroups() throws IOException {
+	
+	/**
+	 * Prints all book-groups from MySQL. Function will use ready-made @NamedQuery to print all the 
+	 * available book-groups with their ID and name. User will then be prompted to select one of the
+	 * book-groups by their unique ID for further querying.
+	 * @return 1 book-group's unique ID | zero if input cannot be parsed to integer
+	 */
+	public static int printBookGroups() {
     List<BookGroup> results = emf.createEntityManager().createNamedQuery("BookGroup.findAll").getResultList();
 	  System.out.println("Valitse kirjaryhmä numerolla:");
 	  for (BookGroup bg : results) {
@@ -75,10 +96,19 @@ public class JPAsimplekoe {
       return Integer.parseInt(reader.readLine());
     } catch (NumberFormatException fe) {
       return 0;
-    }	    
+    }	catch (IOException ioe) {
+      return 0;
+    }
 	}
 	
-	public static int printBooks() throws IOException {
+	
+  /**
+   * Prints all books from MySQL. Function will use ready-made @NamedQuery to print all the 
+   * available books with their ID, author and title. User will then be prompted to select one of the
+   * books by their unique ID for further querying.
+   * @return 1 book's unique ID | zero if input cannot be parsed to integer
+   */
+	public static int printBooks() {
 	  List<Book> res = emf.createEntityManager().createNamedQuery("Book.findAll").getResultList();
 	  System.out.println("Valitse kirja numerolla:");
 	  for (Book b : res) {
@@ -89,44 +119,74 @@ public class JPAsimplekoe {
 	    return Integer.parseInt(reader.readLine());
 	  } catch (NumberFormatException fe) {
 	    return 0;
+	  } catch (IOException ioe) {
+	    return 0;
 	  }
 	}
 	
-	public static void createBook() throws Exception {
+	/**
+	 * Create new book. Function which will create a new book to MySQL's book-table.
+	 * User input will be used to determine the information for the book. All the values
+	 * will be checked so that the book's values will be valid. Book's book-group will be
+	 * determined via another function. If all the values are valid, push new book to the table.
+	 */
+	public static void createBook() {
 	  
-	  System.out.print("Kirjan tekijän nimi: ");
-	  String author = reader.readLine();
-	  System.out.print("Kirjan nimi: ");
-	  String title = reader.readLine();
+	  String author = null;
+	  String title = null;
+	  
+	  // Get user's input for new book's information
+	  try {
+	    System.out.print("Kirjan tekijän nimi: ");
+	    author = reader.readLine();
+	    System.out.print("Kirjan nimi: ");
+	    title = reader.readLine();
+	  } catch (IOException ioe) {
+	    System.out.println(ioe);
+	  }
 		
+	  // Create objects from book and book-group
 	  Book b = new Book();
 		BookGroup bg = new BookGroup();
 		
+		// If author's and title's values aren't empty, proceed
 		if (!author.replaceAll("\\s+", "").isEmpty() && !title.replaceAll("\\s+", "").isEmpty()) {
+		  // Select book-group for the book
 		  Integer bookGroupId = printBookGroups();
 		  
+		  // If user selected atleast one (viable) book-group for the book via ID
 		  if (bookGroupId != 0 && bookGroupId != null) {
 		    bg.setId(bookGroupId);
 		    b.setBookGroup(bg);
 		  }
 		  
+		  // Push new values into a book-object
 		  b.setAuthor(author);
 		  b.setTitle(title);
 		  
 		  initTransaction();
 		  
+		  // Begin query and insert new values to MySQL -table with ORM
 		  tx.begin();
 		  em.persist(b);
 		  tx.commit();
 		}
 	}
 	
-	
-	public static void deleteBook() throws Exception {
+
+	/**
+	 * Delete's book from table via it's ID. Book will first be chosen via
+	 * another function. If the ID-value isn't empty, it'll be deleted.
+	 */
+	public static void deleteBook() {
+	  // Pick the ID of the book from another function and place it into variable
 	  Integer id = printBooks();
+	  // If the value is viable and not empty
 	  if (id != 0 && id != null) {
+	    // Find the book from book-class via unique ID -value
 	    Book b = em.find(Book.class, id);
 	    try {
+	      // Begin query and delete book from MySQL -table with ORM
 	      initTransaction();
 	      tx.begin();
 	      em.remove(b);
@@ -137,10 +197,26 @@ public class JPAsimplekoe {
 	  }
 	}
 	
-	public static void createBookGroup() throws Exception {
+	
+	/**
+	 * Creates new book-group. It'll be created via user input. If name is valid and isn't empty
+	 * it'll be pushed into MySQL -table.
+	 */
+	public static void createBookGroup() {
+	  
+	  String name = null;
 	  System.out.print("Anna kirjaryhmälle nimi: ");
-	  String name = reader.readLine();
+	  
+	  // Fetch the new name via user input
+	  try {
+	    name = reader.readLine();
+	  } catch (IOException ioe) {
+	    System.out.println(ioe);
+	  }
+	  
+	  // If new name is valid and isn't empty
 	  if (name != null && !name.replaceAll("\\s+", "").isEmpty()) {
+	    
 	    BookGroup bg = new BookGroup();
 	    bg.setName(name);
 	    
@@ -151,68 +227,98 @@ public class JPAsimplekoe {
 	  }
 	}
 	
-	public static void deleteBookGroup() throws Exception {
-	  System.out.println("\n**VAROITUS: Poistaa myös kirjaryhmään kuuluvat kirjat!**\n");
+	
+	/**
+	 * Deletes book-group and all the books which are attached to that book-group.
+	 * First the wanted book-group's ID will be fetched via external function. If the 
+	 * value is valid and not empty, find all the books which has the same book-group's ID
+	 * attached to them. Then through a for-loop delete them one-by-one. Lastly delete
+	 * the book-group.
+	 */
+	public static void deleteBookGroup() {
+	  
+	  System.out.println("\n!! VAROITUS: Poistaa myös kirjaryhmään kuuluvat kirjat! !!\n");
+	  // Get the wanted book's ID via external function and place it into a variable
 	  Integer id = printBookGroups();
+	  
+	  // If the value is viable
 	  if (id != 0 && id != null) {
 	    try {
+	      // Find the row via ID
 	      BookGroup bg = em.find(BookGroup.class, id);
-	      BookGroup bb = new BookGroup();
-	      bb.setId(id);
-	      initTransaction();
+	      // Fetch all the books which has the same book-group ID attached to them
 	      List<Book> books = emf.createEntityManager().createNamedQuery("Book.findByBookGroup").setParameter("id", bg).getResultList();
+
+	      initTransaction();
 	      tx.begin();
 	      
+	      // Loop all the founded books. Find their object's and them remove them via em -object
 	      for (Book book: books) {
 	        Book b = em.find(Book.class, book.getId());
 	        em.remove(b);
 	      }
-	      
+	      // Lastly remove the book-group
 	      em.remove(bg);
+	      // Commit changes
 	      tx.commit();
+	      
 	    } catch (IllegalArgumentException iae) {
 	      System.out.println(iae);
 	      System.out.println("\n// Kirjaryhmää ID:llä: [" + id + "] ei voitu poistaa. Ei löydetty\n");
 	    }
 	  }
 	}
+	 
 	
-  public static void showBooks() throws Exception {
-
-		List<Book> res1 = emf.createEntityManager().createNamedQuery("Book.findAll").getResultList();
+	/**
+	 * Finds all the books from the table and print them into a table -structure.
+	 */
+  public static void showBooks() {
+    
+    // Fetch all the books
+		List<Book> books = emf.createEntityManager().createNamedQuery("Book.findAll").getResultList();
 		System.out.println("+---------------+\n" +
 		                   "|    KIRJAT     |");
 		
-		String leftAlignFormat = "| %-20s | %-30s | %-30s |%n";
-		System.out.format("+---------------+------+--------------------------------+--------------------------------+%n");
-		System.out.format("| Kirjailija           | Kirja                          | Kirjaryhmä                     |%n");
-		System.out.format("+----------------------+--------------------------------+--------------------------------+%n");
+		// Column names
+		String leftAlignFormat = "| %-30s | %-40s | %-40s |%n";
+		System.out.format("+---------------+----------------+------------------------------------------+------------------------------------------+%n");
+		System.out.format("| Kirjailija                     | Kirja                                    | Kirjaryhmä                               |%n");
+		System.out.format("+--------------------------------+------------------------------------------+------------------------------------------+%n");
 		
-		for (Book b : res1) {
+		// Loop through all books the print them row-by-row
+		for (Book b : books) {
 		  System.out.format(leftAlignFormat, b.getAuthor(), b.getTitle(), 
 		    b.getBookGroup() != null ? b.getBookGroup().getName() : "");
 		}
 		
-		System.out.format("+----------------------+--------------------------------+--------------------------------+%n");
-		System.out.format("| KIRJOJEN MÄÄRÄ       | " + res1.size() + "                                                              |%n");
-		System.out.format("+----------------------+-----------------------------------------------------------------+%n");
+		// Prints the amount of books in the table
+		System.out.format("+--------------------------------+------------------------------------------+------------------------------------------+%n");
+		System.out.format("| KIRJOJEN MÄÄRÄ                 | " + books.size() + "                                                                                  |%n");
+		System.out.format("+--------------------------------+-------------------------------------------------------------------------------------+%n");
 		
+		// Print the columns for book-groups' names and the amount of books in each of the group
 		System.out.println("+---------------+\n" +
 		                   "|  KIRJARYHMÄT  |");
-		
+		leftAlignFormat = "| %-30s | %-15s |%n";
 		System.out.format("+---------------+----------------+-----------------+%n");
 		System.out.format("| Nimi                           | Kirjoja (kpl)   |%n");
 		System.out.format("+--------------------------------+-----------------+%n");
 		
-		leftAlignFormat = "| %-30s | %-15s |%n";
-		List<Object[]> res2 = emf.createEntityManager().createNamedQuery("BookGroup.findAndCount").getResultList();
-		for (Object[] res: res2) {
+		// Fetch all the book-groups
+		List<Object[]> bookGroups = emf.createEntityManager().createNamedQuery("BookGroup.findAndCount").getResultList();
+		// Loop through all book-groups to print them row-by-row
+		for (Object[] res: bookGroups) {
 		  System.out.format(leftAlignFormat, (String) res[0], ((Number) res[1]).intValue());
 		}
 		System.out.format("+--------------------------------+-----------------+%n");
 	}
 
 	
+  /**
+   * Main-function which'll be executed when program starts. This a while -loop program so it loops through the same 
+   * block as many times as the user wants.
+   */
 	public static void main(String args[]) {
 	  
 	  boolean run = true;
@@ -250,6 +356,7 @@ public class JPAsimplekoe {
 	          break;
 	        case "6":
 	          System.out.println("Suljetaan..");
+	          System.out.print("--- Sovellus suljettu. Näkemisiin ---");
 	          run = false;
 	        default:
 	          continue;
