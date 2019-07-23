c92d5764fbdadb3de4111eeb8bd814686bd4eeac
SamuliRukkila <samuli.rukkila@hotmail.fi>
2019-07-23 19:11:44 +0300

    Lisätty token-validointi tehtävään 3.3

--- part-3/exercise-3/src/main/java/jpa_rest_api/CustomerAPI.java
+++ part-3/exercise-3/src/main/java/jpa_rest_api/CustomerAPI.java
@@ -1,8 +1,14 @@
 package jpa_rest_api;
 
+import java.security.Key;
+import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+
 import javax.ejb.EJB;
+import javax.transaction.Transactional;
 import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -13,20 +19,50 @@ import javax.ws.rs.core.Application;
 import javax.ws.rs.core.GenericEntity;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.HeaderParam;
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SignatureException;
 
 /**
  * Customer-API which includes REST-API. It'll use JSON as a type to pass data.
  * Request-types (Get, Post, Delete) will be created using JAX-RS annotation.
  * @path (/customersapi/customer/)
  * @author samuli
+ * @param <JSONObject>
  */
 @ApplicationPath("customersapi")
 @Path("customer")
+@Transactional
 @Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class CustomerAPI extends Application {
   
   // Inject Customer-Service to REST-API
   @EJB private CustomerService cs;
+
+  private static final Key signingKey = new SecretKeySpec(
+      DatatypeConverter.parseBase64Binary("ExampleKey"), SignatureAlgorithm.HS512.getJcaName()
+  );
+  
+  
+  /**
+   * Validates user's token.
+   * @param authToken - token which'll be validated
+   * @return boolean value representing of token was valid/invalid
+   */
+  public boolean validateToken(String authToken) {
+    try {
+      Jwts.parser().setSigningKey(signingKey).parseClaimsJws(authToken);
+      return true;
+    } catch (SignatureException e) {
+      System.out.println(e);
+      return false;
+    }
+  }
   
   
   /**
@@ -43,6 +79,35 @@ public class CustomerAPI extends Application {
       return false;
     }
   }
+  
+  
+  /**
+   * Function to login user using static values ("123" & "123"). Token will be
+   * created for the user if the values are same. 
+   * @param c - User's email & password
+   * @return Token; if wrong credentials returns error-message
+   */
+  @POST
+  @Path("/login")
+  public Response login(Customer c) {
+    if (c.getEmail().equals("123") && c.getSalasana().equals("123")) {
+      try {
+        String token = Jwts.builder()
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + 3600000))
+            .signWith(SignatureAlgorithm.HS512, signingKey)
+            .compact();
+        return Response.status(Response.Status.OK)
+            .entity("Tervetuloa " + c.getEmail() + ". Tässä token: \n" + token)
+            .build();
+      } catch (Exception e) {
+        return Response.status(500).entity(e).build();
+      }
+    } else {
+      return Response.status(401).entity("Väärä tunnus ja/tai salasana").build();
+    }
+  }
+  
   
   
   /**
@@ -103,7 +168,12 @@ public class CustomerAPI extends Application {
    */
   @DELETE
   @Path("{id:}")
-  public Response deleteCustomer(@PathParam("id") String id) {
+  public Response deleteCustomer(@PathParam("id") String id, @HeaderParam("token") String token) {
+    
+    if (validateToken(token) == false) {
+      return Response.status(401).entity("Väärä token").build();
+    }
+    
     // Validate parameter
     if (checkParameter(id) == false) {
       return Response.status(400)
